d67ae868c8cf6bdc4eaddf912d6b420fc437b4e6
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-03 14:22:21 +0100

    delete cars controllers - add auction controller

---
+++ Controllers/AuctionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarBidMVC.Controllers
+{
+    public class AuctionController : Controller
+    {
+        //  auction/damaged
+        public IActionResult Damaged()
+        {
+            return View();
+        }
+
+        //  auction/undamaged
+        public IActionResult Undamaged()
+        {
+            return View();
+        }
+    }
+}
