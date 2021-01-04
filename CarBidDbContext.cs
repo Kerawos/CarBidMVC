b88304b5496acaca81e966a970277593064be676
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-04 18:29:38 +0100

    db context

---
+++ CarBidDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarBidMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarBidMVC
+{
+    public class CarBidDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+
+}
