f5ecb027493f41280caaa70a220611123433a17d
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-06 13:06:36 +0100

    ICarDataDamaged+Undamaged

--- CarLibrary/ICarData.cs
+++ CarLibrary/ICarData.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using CarBidMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CarBidMVC.Models
+namespace CarBidMVC.CarLibary
 {
-    interface ICarData
+    public interface ICarData
     {
         IEnumerable<Car> GetAll();
         IEnumerable<Car> GetAllByName(string name);
---
+++ CarLibrary/ICarDataDamaged.cs
@@ -0,0 +1,14 @@
+﻿using CarBidMVC.CarLibary;
+using CarBidMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarBidMVC.CarLibrary
+{
+    public interface ICarDataDamaged : ICarData
+    {
+        IEnumerable<Car> GetAllDamaged();
+    }
+}
