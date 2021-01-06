0cc1e79827813ca04d3e1eb558c30d4833f635ee
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-06 11:32:49 +0100

    image model

---
+++ Models/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarBidMVC.Models
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        public string URL { get; set; }
+
+        public Image(string url)
+        {
+            this.URL = url;
+        }
+
+        public Image()
+        {
+
+        }
+    }
+}
