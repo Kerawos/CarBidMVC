5c5131e4d3abc3364df6a6210b464becb4c793d4
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-07 18:52:50 +0100

    services, add scoped

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using CarBidMVC.CarDb;
+using CarBidMVC.CarLibary;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,7 +30,7 @@ namespace CarBidMVC
             {
                 options.UseSqlServer(Configuration.GetConnectionString("CarBidDb"));
             });
-            //services.AddSingleton<>
+            services.AddScoped<ICarData, CarDataSQL>();
             services.AddControllersWithViews();
         }
 
